258be45ba7f1c301fb2e0cf5db61ca04bccae7aa
namhan2410619-cyber <namhan2410619@ggh.goe.go.kr>
2025-11-30 11:29:03 +0900

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -1,21 +1,12 @@
 import datetime
-# IoT 메시지 전송(MQTT)
-import paho.mqtt.client as mqtt
 
-def send_alarm_signal():
-    client = mqtt.Client()
-    client.connect("broker.hivemq.com", 1883)
-    client.publish("iot/alarm", "wake_up")
-    client.disconnect()
-
-def calculate_alarm_time(prep, commute):
+def calculate_alarm_time(prep_time, commute_time, school_hour=9, school_minute=0):
     now = datetime.datetime.now()
-    school_time = now.replace(hour=9, minute=0, second=0)
+    school_time = now.replace(hour=school_hour, minute=school_minute, second=0)
 
-    wake_time = school_time - datetime.timedelta(minutes=(prep + commute))
+    wake_time = school_time - datetime.timedelta(minutes=(prep_time + commute_time))
 
     if wake_time < now:
-        wake_time += datetime.timedelta(days=1)  # 다음날 시간으로 보정
+        wake_time += datetime.timedelta(days=1)
 
-    send_alarm_signal()  # IoT 알람 전송
     return wake_time.strftime("%H:%M")
