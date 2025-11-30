46bb4ff79a4e2bf2650f081b53f7080a49163ade
namhan2410619-cyber <namhan2410619@ggh.goe.go.kr>
2025-11-30 11:35:31 +0900

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -1,12 +1,15 @@
 import datetime
 
 def calculate_alarm_time(prep_time, commute_time, school_hour=9, school_minute=0):
+    prep_time = int(prep_time)
+    commute_time = int(commute_time)
+    
     now = datetime.datetime.now()
     school_time = now.replace(hour=school_hour, minute=school_minute, second=0)
-
+    
     wake_time = school_time - datetime.timedelta(minutes=(prep_time + commute_time))
-
+    
     if wake_time < now:
         wake_time += datetime.timedelta(days=1)
-
+    
     return wake_time.strftime("%H:%M")
